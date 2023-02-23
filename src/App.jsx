633b05991549c4fe478235c66729ca680c283716
George Colletti Barajas <miracle@dreamsmith.net>
2023-02-23 06:40:29 -0600

    add management of books over api with axios and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
 
 const App = () => {
   const [books, setBooks] = useState([]);
 
-  const editBookById = (id, newTitle) => {
-    const updatedBooks = books.map((book) => {
-      return (book.id === id && { ...book, title: newTitle }) || book;
-    });
+  const fetchBooks = async () => {
+    const response = await axios.get("http://localhost:3001/books");
 
-    setBooks(updatedBooks)
+    setBooks(response.data);
   };
 
-  const deleteBookById = (id) => {
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+
+    const updatedBooks = books.map((book) => {
+      return (book.id === id && { ...book, ...response.data }) || book;
+    });
+
+    setBooks(updatedBooks);
+  };
+
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
+
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
@@ -21,13 +34,18 @@ const App = () => {
     setBooks(updatedBooks);
   };
 
-  const createBook = (title) => {
-    const updatedBooks = [
-      ...books,
-      { id: Math.round(Math.random() * 9999), title },
-    ];
+  const createBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title,
+    });
+
+    const updatedBooks = [...books, response.data];
     setBooks(updatedBooks);
   };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
 
   return (
     <div className="app">
